4689275236f9715d5ee1a506127f5fd977401725
AnthonySinitsa <itonyemail@gmail.com>
2023-10-24 02:34:22 +0000

    command

--- body.cpp
+++ body.cpp
@@ -3,7 +3,17 @@
 
 // Function to print the Tic Tac Toe board
 void drawBoard(char *spaces){
-
+    std::cout << '\n';
+    std::cout << "     |     |     " << '\n';
+    std::cout << "   " << spaces[0] << "  |  " << spaces[1] << "   |    " << spaces[2] << "  " << '\n';
+    std::cout << "_____|_____|_____" << '\n';
+    std::cout << "     |     |      " << '\n';
+    std::cout << "   " << spaces[3] << "  |  " << spaces[4] << "   |    " << spaces[5] << "  " << '\n';
+    std::cout << "_____|_____|_____" << '\n';
+    std::cout << "     |     |      " << '\n';
+    std::cout << "   " << spaces[6] << "  |  " << spaces[7] << "   |    " << spaces[8] << "  " << '\n';
+    std::cout << "     |     |      " << '\n';
+    std::cout << '\n';
 }
 void playerMove(char *spaces, char player){
 
@@ -12,8 +22,8 @@ void computerMove(char *spaces, char computer){
 
 }
 bool checkWinner(char *spaces, char player, char computer){
-    return true;
+    return 0;
 }
 bool checkTie(char *spaces){
-    return true;
+    return 0;
 }
--- ttt.cpp
+++ ttt.cpp
@@ -4,4 +4,10 @@
 #include "body.h"
 
 int main() {
+    char spaces[9] = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    char player = 'X';
+    char computer = 'O';
+    bool running = true;
+    drawBoard(spaces);
+    
 }
