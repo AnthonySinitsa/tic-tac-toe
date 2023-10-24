bb6e8e72b4ebbcf280e592edfe0c8ea3661d762c
AnthonySinitsa <itonyemail@gmail.com>
2023-10-24 02:35:46 +0000

    command

--- body.cpp
+++ body.cpp
@@ -5,13 +5,13 @@
 void drawBoard(char *spaces){
     std::cout << '\n';
     std::cout << "     |     |     " << '\n';
-    std::cout << "   " << spaces[0] << "  |  " << spaces[1] << "   |    " << spaces[2] << "  " << '\n';
+    std::cout << "  " << spaces[0] << "  |  " << spaces[1] << "  |    " << spaces[2] << "  " << '\n';
     std::cout << "_____|_____|_____" << '\n';
     std::cout << "     |     |      " << '\n';
-    std::cout << "   " << spaces[3] << "  |  " << spaces[4] << "   |    " << spaces[5] << "  " << '\n';
+    std::cout << "  " << spaces[3] << "  |  " << spaces[4] << "  |    " << spaces[5] << "  " << '\n';
     std::cout << "_____|_____|_____" << '\n';
     std::cout << "     |     |      " << '\n';
-    std::cout << "   " << spaces[6] << "  |  " << spaces[7] << "   |    " << spaces[8] << "  " << '\n';
+    std::cout << "  " << spaces[6] << "  |  " << spaces[7] << "  |    " << spaces[8] << "  " << '\n';
     std::cout << "     |     |      " << '\n';
     std::cout << '\n';
 }
